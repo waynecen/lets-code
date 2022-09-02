0ee37683a520d78c2fb4594c72405b25288bd04d
Wayne <wayne.cen@gmail.com>
2022-09-02 15:55:20 -0400

    Add variables, update position of black keys

--- components/PianoKey.tsx
+++ components/PianoKey.tsx
@@ -3,13 +3,23 @@ import { pianoKeyData } from "../data/pianoKeyData.tsx";
 
 export default function PianoKey() {
 	const pianoKeys = pianoKeyData.map((key, index) => {
-		return (
+		return key.isColor ? (
 			<div
 				key={index}
-				className={key.isColor ? styles.colorKey : styles.blackKey}
+				className={styles.colorKey}
 				style={{ backgroundColor: key.backgroundColor }}
 			>
 				<p className={styles.letterBox}>{key.letter}</p>
+			</div>
+		) : (
+			<div className={styles.wrapper}>
+				<div
+					key={index}
+					className={styles.blackKey}
+					style={{ backgroundColor: key.backgroundColor }}
+				>
+					<p className={styles.letterBox}>{key.letter}</p>
+				</div>
 			</div>
 		);
 	});
