5501cd11ee39593ed5ad52daf2617963bf9c3cfc
Wayne <wayne.cen@gmail.com>
2022-09-07 20:50:00 -0400

    Add audio files, update styling, add onClick to pianoKey

--- components/PianoKey.tsx
+++ components/PianoKey.tsx
@@ -1,27 +1,26 @@
 import styles from "./pianokey.module.scss";
 import { pianoKeyData } from "../data/pianoKeyData";
 import ConditionalWrapper from "./ConditionalWrapper";
+import useSound from "use-sound";
 
 export default function PianoKey() {
-	const pianoKeys = pianoKeyData.map((key, index) => {
-		let audio = new Audio("/music/elevator.mp3");
-
-		const start = () => {
-			audio.play();
-		};
+	const pianoKeys = pianoKeyData.map((key) => {
+		const [play] = useSound(`/music/${key.audioSource}`);
 
 		return (
 			<ConditionalWrapper
-				key={index}
+				key={key.id}
 				condition={key.isColor}
 				wrapper={(children) => <div className={styles.wrapper}>{children}</div>}
 			>
 				<div
 					className={key.isColor ? styles.colorKey : styles.blackKey}
 					style={{ backgroundColor: key.backgroundColor }}
+					onClick={play}
 				>
-					<p className={styles.letterBox}>{key.letter}</p>
-					<button onClick={start}>Play</button>
+					<p className={key.isColor ? styles.letterBox : styles.blackLetterBox}>
+						{key.letter}
+					</p>
 				</div>
 			</ConditionalWrapper>
 		);
--- data/pianoKeyData.tsx
+++ data/pianoKeyData.tsx
@@ -1,49 +1,57 @@
 export const pianoKeyData: {
+	id: string;
 	letter: string;
 	isColor: boolean;
 	audioSource: string;
 	backgroundColor: string;
 }[] = [
 	{
+		id: "01",
 		letter: "M",
 		isColor: true,
-		audioSource: "/",
+		audioSource: "/01.mp3",
 		backgroundColor: "#91C3CE",
 	},
 	{
+		id: "2",
 		letter: "U",
 		isColor: false,
-		audioSource: "/",
+		audioSource: "/02.mp3",
 		backgroundColor: "#0D0D0D",
 	},
 	{
+		id: "3",
 		letter: "B",
 		isColor: true,
-		audioSource: "/",
+		audioSource: "/03.mp3",
 		backgroundColor: "#FAC670",
 	},
 	{
+		id: "4",
 		letter: "A",
 		isColor: true,
-		audioSource: "/",
+		audioSource: "/04.mp3",
 		backgroundColor: "#F5E1BE",
 	},
 	{
+		id: "5",
 		letter: "S",
 		isColor: false,
-		audioSource: "/",
+		audioSource: "/05.mp3",
 		backgroundColor: "#0D0D0D",
 	},
 	{
+		id: "6",
 		letter: "I",
 		isColor: true,
-		audioSource: "/",
+		audioSource: "/06.mp3",
 		backgroundColor: "#F37925",
 	},
 	{
+		id: "7",
 		letter: "C",
 		isColor: true,
-		audioSource: "/",
+		audioSource: "/07.mp3",
 		backgroundColor: "#C0A8E6",
 	},
 ];
