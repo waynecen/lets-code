7352ec6ac00ecb37bd0526e480b482505ccdc79d
Wayne <wayne.cen@gmail.com>
2022-09-12 01:20:20 -0400

    Add hover state to keys, add keydown function

--- components/PianoKey.tsx
+++ components/PianoKey.tsx
@@ -2,10 +2,25 @@ import styles from "./pianokey.module.scss";
 import { pianoKeyData } from "../data/pianoKeyData";
 import ConditionalWrapper from "./ConditionalWrapper";
 import useSound from "use-sound";
+import { useEffect } from "react";
 
 export default function PianoKey() {
 	const pianoKeys = pianoKeyData.map((key) => {
-		const [play] = useSound(`/music/${key.audioSource}`);
+		const handleKeyDown = (e) => {
+			if (e.key === key.letter) {
+				play();
+			}
+		};
+
+		const [play] = useSound(`/music/${key.audioSource}`, {
+			volume: 0.7,
+			playbackRate: 0.7,
+			interrupt: true,
+		});
+
+		useEffect(() => {
+			document.addEventListener("keydown", handleKeyDown, true);
+		}, [play]);
 
 		return (
 			<ConditionalWrapper
@@ -14,8 +29,8 @@ export default function PianoKey() {
 				wrapper={(children) => <div className={styles.wrapper}>{children}</div>}
 			>
 				<div
-					className={key.isColor ? styles.colorKey : styles.blackKey}
 					style={{ backgroundColor: key.backgroundColor }}
+					className={`${key.isColor ? styles.colorKey : styles.blackKey}`}
 					onClick={play}
 				>
 					<p className={`${styles.letterBox} ${key.isColor ? "" : styles.black}`}>
