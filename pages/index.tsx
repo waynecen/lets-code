9a4c6a3ae77f52560b05f8861d2179623755073b
Wayne <wayne.cen@gmail.com>
2022-09-14 19:34:10 -0400

    Fix deployment type error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import BoxLink from "../components/BoxLink";
+import BoxLink from "../components/BoxLink.tsx";
 import utils from "../styles/utils.module.scss";
 
 export default function Home() {
