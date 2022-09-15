a94c3536ae07ec290b192f888978356ce616b8f3
Wayne <wayne.cen@gmail.com>
2022-09-15 01:09:24 -0400

    Fix cases causing pathing errors for deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utils from "../styles/utils.module.scss";
 // @ts-ignore
-import BoxLink from "../components/boxLink";
+import BoxLink from "../components/BoxLink";
 
 export default function Home() {
 	return (
@@ -12,7 +12,7 @@ export default function Home() {
 			</Head>
 			<section className={utils.container}>
 				<BoxLink route="mubasic">Mubasic</BoxLink>
-				<BoxLink>Coming Soon</BoxLink>
+				<BoxLink route="">Coming Soon</BoxLink>
 			</section>
 		</Layout>
 	);
