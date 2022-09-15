070696a9988bee9d4b2f0bd59da89a394fbac7f5
Wayne <wayne.cen@gmail.com>
2022-09-15 01:02:44 -0400

    TS ignore to BoxLink component, causing Type Error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utils from "../styles/utils.module.scss";
+// @ts-ignore
 import BoxLink from "../components/boxLink";
 
 export default function Home() {
